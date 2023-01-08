25cee12b59a32fffa3cec8627e550c92054bdb51
elandalouss <ayoub.el.andaloussi02@gmail.com>
2023-01-08 21:59:34 +0100

    module 00 is almost good

--- cpp module 00/ex01/ex01.cpp
+++ cpp module 00/ex01/ex01.cpp
@@ -24,24 +24,19 @@ class Contact{
 		}
 };
 
+int i = 0;
+
 void putDot(std::string	str)
 {
 	if (str.length() > 10)
 		std::cout << ".";
 }
 
-int main ()
-{
-	std::string		cmd;
-	Contact			data[8];
-	int				i, index;
-
-	i = 0;
-	while(true)
-	{
-		std::cin >> cmd;
-		if (cmd.compare("ADD") == 0)
-		{
+class PhoneBook{
+	private :
+		Contact	cnt[8];
+	public :
+		void fillClass() {
 			std::string firstName, lastName, nickName;
 			int	phoneNumber;
 			std::cout << "Enter your firstName : ";
@@ -57,30 +52,50 @@ int main ()
 				std::cout << "should entre a number" << std::endl;
 				exit(0);
 			}
-			data[i].setData(firstName, lastName, nickName, phoneNumber);
+			cnt[i].setData(firstName, lastName, nickName, phoneNumber);
 			if (i == 7)
 				i = -1;
 			i++;
 		}
+
+		void getInfo(int index)
+		{
+			std::cout << index << "|" << std::setw(10) << cnt[index].getFirstName().substr(0, 9);
+			putDot(cnt[index].getFirstName());
+			std::cout << "|" << std::setw(10) << cnt[index].getLastName().substr(0, 9);
+			putDot(cnt[index].getLastName());
+			std::cout << "|" << std::setw(10) << cnt[index].getNeckNameName().substr(0, 9);
+			putDot(cnt[index].getNeckNameName());
+			std::cout << "|" << std::endl;
+		}
+
+};
+
+
+int main()
+{
+	std::string		cmd;
+	PhoneBook		data;
+	int				i, index;
+
+	i = 0;
+	while(true)
+	{
+		std::cin >> cmd;
+		if (cmd.compare("ADD") == 0)
+			data.fillClass();
 		if (cmd.compare("SEARCH") == 0)
 		{
+
 			std::cout << "Enter The Index Of The Contact : ";
 			std::cin >> index;
 			if (std::cin.fail())
 			{
 				std::cout << "should entre a number" << std::endl;
-				exit(0);
+				return (0);
 			}
 			if (index <= 7)
-			{
-				std::cout << index << " | " << std::setw(10) << data[index].getFirstName().substr(0, 9);
-				putDot(data[index].getFirstName());
-				std::cout << " | " << std::setw(10) << data[index].getLastName().substr(0, 9);
-				putDot(data[index].getLastName());
-				std::cout << " | " << std::setw(10) << data[index].getNeckNameName().substr(0, 9);
-				putDot(data[index].getNeckNameName());
-				std::cout << std::endl;
-			}
+				data.getInfo(index);
 			else
 				std::cout << "max contact can store is 7" << std::endl;
 		}
