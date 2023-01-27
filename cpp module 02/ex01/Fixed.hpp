42707082b6dd9e907446df43492799521aa50408
elandalouss <ayoub.el.andaloussi02@gmail.com>
2023-01-27 21:23:21 +0100

    test module 02 and push it

--- cpp module 02/ex01/Fixed.hpp
+++ cpp module 02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: aelandal <aelandal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 05:10:45 by aelandal          #+#    #+#             */
-/*   Updated: 2023/01/26 05:10:46 by aelandal         ###   ########.fr       */
+/*   Updated: 2023/01/27 17:00:28 by aelandal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ class Fixed {
         Fixed (const int num);
         Fixed (const float fnum);
 		~Fixed();
-		int getRawBits(void) const;
-        void setRawBits(int const raw);
-        float toFloat(void) const;
-        int toInt(void) const;
+		int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+        float   toFloat(void) const;
+        int     toInt(void) const;
 };
 
 std::ostream& operator<<(std::ostream& os, const Fixed& obj);
--- cpp module 02/ex01/main.cpp
+++ cpp module 02/ex01/main.cpp
@@ -6,41 +6,38 @@
 /*   By: aelandal <aelandal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 05:10:41 by aelandal          #+#    #+#             */
-/*   Updated: 2023/01/26 23:28:08 by aelandal         ###   ########.fr       */
+/*   Updated: 2023/01/27 19:38:35 by aelandal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
 int main(void) {
-    // Fixed a;
-    // Fixed const b(10.2);
+    Fixed a;
+    Fixed const b(10);
     Fixed const c(42.42f);
-    // Fixed const d(b);
+    Fixed const d(b);
 
-    // a = Fixed(1234.4321f);
+    a = Fixed(1234.4321f);
 
-    std::cout << c.toInt() << std::endl;
-    std::cout << c.toFloat() << std::endl;
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
 
-    // std::cout << "a is " << a << std::endl;
-    // std::cout << "b is " << b << std::endl;
-    // std::cout << "c is " << c << std::endl;
-    // std::cout << "d is " << d << std::endl;
+    std::cout << std::endl << "====>  int  numbers" << std::endl << std::endl;
 
-    // std::cout << std::endl << "====>  int  numbers" << std::endl << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
-    // std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-    // std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-    // std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-    // std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    std::cout << std::endl << "====>  float numbers" << std::endl << std::endl;
 
-    // std::cout << std::endl << "====>  float numbers" << std::endl << std::endl;
-
-    // std::cout << "a is " << a.toFloat() << " as float" << std::endl;
-    // std::cout << "b is " << b.toFloat() << " as float" << std::endl;
-    // std::cout << "c is " << c.toFloat() << " as float" << std::endl;
-    // std::cout << "d is " << d.toFloat() << " as float" << std::endl << std::endl;
+    std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+    std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+    std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+    std::cout << "d is " << d.toFloat() << " as float" << std::endl << std::endl;
 
     return 0;
 }
