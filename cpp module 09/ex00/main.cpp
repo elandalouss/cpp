caec131ff54d543f53b6fd3a781caf4837c69d98
elandalouss <ayoub.el.andaloussi02@gmail.com>
2023-04-06 16:18:36 +0000

    shuiiii

--- cpp module 09/ex00/main.cpp
+++ cpp module 09/ex00/main.cpp
@@ -6,26 +6,11 @@
 /*   By: aelandal <aelandal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:58:36 by aelandal          #+#    #+#             */
-/*   Updated: 2023/04/06 12:02:50 by aelandal         ###   ########.fr       */
+/*   Updated: 2023/04/06 16:05:54 by aelandal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-
-// int check_pipe(std::string line) {
-// 	std::size_t pos = line.find("|");
-// 	if (pos == std::string::npos) {
-// 		std::cerr << "ERROR : input should be liks [date | value]" << std::endl;	
-// 		return -1;
-// 	} else if (pos != std::string::npos) {	
-// 	}
-// 	return 1;
-// }
-
-// void check_errors(std::string line) {
-// 	if (check_pipe(line) == -1)
-// 		return ;
-// }
 
 void skipAllWhiteSpaces(std::string &str) {
 	for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
@@ -34,23 +19,45 @@ void skipAllWhiteSpaces(std::string &str) {
 	}
 }
 
+bool YYYY_MM_DD(std::string date) {
+	std::string year;
+	std::string month;
+	std::string day;
+	
+	year = date.substr(0, 4);
+	month = date.substr(5, 2);
+	day = date.substr(8, 2);
+	if (atol(year.c_str()) <= 0 || atol(month.c_str()) < 1 || atol(month.c_str()) > 12 
+		|| atol(day.c_str()) < 1 || atol(day.c_str()) > 31 || date[4] != '-' || date[7] != '-')
+		return 0;
+	return 1;
+}
+
+bool check_value(float value) {
+	if (value < 0 || value > 1000)
+		return 0;
+	return 1;
+}
+
 std::map<std::string, float> storeIntoMap(std::ifstream &infile, std::string delemiter) {
 	std::map<std::string, float> my_map;
-	(void)delemiter;
 	std::string line;
 	while (getline(infile, line)) {
 		skipAllWhiteSpaces(line);
-		// check_errors(line_infile);
 		std::size_t pos = line.find(delemiter);
 		if (pos != std::string::npos){
 			std::string str = line.substr(pos + 1);
-			float a = atof(str.c_str());
-			my_map[line.substr(0, pos)] = a;
-		// std::cout << str << std::endl;
-			std::cout << "key : " << line.substr(0, pos) << " value : " << a << std::endl;
+			float value = atof(str.c_str());
+			std::string key = line.substr(0, pos);
+			if (YYYY_MM_DD(key) && check_value(value))
+				my_map[key] = value;
+			else
+				std::cerr << "ERROR : Bed Date or value" << std::endl;
+				
+			// std::cout << "value : " << value << " key : " << key << std::endl;
 		} else
 			std::cout << "Error: bad input" << std::endl;
-	}
+	}  
 	return (my_map);
 }
 
@@ -63,9 +70,11 @@ int main(int ac, char **av) {
 	if (!infile || !dataBase){
 		std::cerr << "ERROR : File Doesn't Open Successfully Lol" << std::endl;
 		return (0);
-	} else {
-		// std::map<std::string, float> dataBaseMap = storeIntoMap(dataBase, ",");
-		std::map<std::string, float> infileMap = storeIntoMap(infile, "|");
+	} else
+		std::map<std::string, float> dataBaseMap = storeIntoMap(dataBase, ",");
+	std::string line;
+	while (getline(infile, line)) {
+		 
 	}
 	return 0;
 }
