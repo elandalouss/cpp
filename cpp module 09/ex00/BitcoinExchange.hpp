2724e3b8a88e0947ff17e48c7d0d6ee4c50344d1
elandalouss <ayoub.el.andaloussi02@gmail.com>
2023-04-08 16:01:16 +0000

    should test it

--- cpp module 09/ex00/BitcoinExchange.hpp
+++ cpp module 09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: aelandal <aelandal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 15:53:40 by aelandal          #+#    #+#             */
-/*   Updated: 2023/04/08 13:12:49 by aelandal         ###   ########.fr       */
+/*   Updated: 2023/04/08 15:59:53 by aelandal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@
 #include <cstring>
 #include <cmath>
 
-
-
+bool isAllDigit(std::string str);
+void skipAllWhiteSpaces(std::string &str);
+bool YYYY_MM_DD(std::string date);
+int check_value(float value);
+std::map<std::string, float> storeIntoMap(std::ifstream &infile, std::string delemiter);
 
 #endif
--- cpp module 09/ex00/main.cpp
+++ cpp module 09/ex00/main.cpp
@@ -6,89 +6,11 @@
 /*   By: aelandal <aelandal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 14:58:36 by aelandal          #+#    #+#             */
-/*   Updated: 2023/04/08 15:51:00 by aelandal         ###   ########.fr       */
+/*   Updated: 2023/04/08 16:00:09 by aelandal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-
-bool isAllDigit(std::string str) {
-	int countDot = 0;
-	for (int i = 0; str[i]; i++) {
-		if (!isdigit(str[i])) {
-			if (str[i] != '.' && str[i] != '-')
-				return 0;
-			else {
-				if (str[i] == '.') {
-					countDot++;
-					if (i == 0 || i == (int)str.length() - 1 || countDot > 1)
-						return 0;
-				}
-			}
-		}
-	}
-	return 1;
-}
-
-void skipAllWhiteSpaces(std::string &str) {
-	for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
-		if (isspace(*it))
-			it = str.erase(it);
-	}
-}
-
-bool YYYY_MM_DD(std::string date) {
-	std::string year;
-	std::string month;
-	std::string day;
-
-	if (date.length() != 10) {
-		std::cerr << "ERROR : check syntax" << std::endl;
-		return 0;
-	}
-	if (date[4] != '-' || date[7] != '-') {
-		std::cerr << "ERROR : check syntax" << std::endl;
-		return 0;
-	}
-	year = date.substr(0, 4);
-	month = date.substr(5, 2);
-	day = date.substr(8, 2);
-	if (!isAllDigit(year) || !isAllDigit(month) || !isAllDigit(day)) {
-		std::cerr << "ERROR : Bad Input => " << date << std::endl;
-		return 0;
-	}
-	if (atol(year.c_str()) <= 0 || atol(month.c_str()) < 1 || atol(month.c_str()) > 12 
-		|| atol(day.c_str()) < 1 || atol(day.c_str()) > 31 || date[4] != '-' || date[7] != '-') {
-		std::cerr << "ERROR : Bad Input => " << date << std::endl;
-		return 0;
-	}
-	return 1;
-}
-
-int check_value(float value) {
-	if (value < 0)
-		return -1;
-	else if (value > 1000)
-		return 0;
-	return 1;
-}
-
-std::map<std::string, float> storeIntoMap(std::ifstream &infile, std::string delemiter) {
-	std::map<std::string, float> my_map;
-	std::string line, str, key;
-	float value;
-	while (getline(infile, line)) {
-		std::size_t pos = line.find(delemiter);
-		if (pos != std::string::npos){
-			str = line.substr(pos + 1);
-			value = strtod(str.c_str(), NULL);
-			key = line.substr(0, pos);
-			my_map[key] = value;
-		} else
-			std::cout << "Error: bad input" << std::endl;
-	}  
-	return (my_map);
-}
 
 int main(int ac, char **av) {
 	if (ac != 2) {
