83f1727ad33107cb78496040617f47d0b59d1ba1
elandalouss <ayoub.el.andaloussi02@gmail.com>
2023-04-12 14:33:00 +0000

    starting the last ex in module 09

--- cpp module 09/ex02/PmergeMe.hpp
+++ cpp module 09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: aelandal <aelandal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 12:44:06 by aelandal          #+#    #+#             */
-/*   Updated: 2023/04/09 12:53:01 by aelandal         ###   ########.fr       */
+/*   Updated: 2023/04/11 12:28:24 by aelandal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,6 @@
 #include <iostream>
 #include <deque>
 #include <vector>
+#include <cstdlib>
 
 #endif
--- cpp module 09/ex02/main.cpp
+++ cpp module 09/ex02/main.cpp
@@ -6,18 +6,118 @@
 /*   By: aelandal <aelandal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 12:47:57 by aelandal          #+#    #+#             */
-/*   Updated: 2023/04/09 15:02:45 by aelandal         ###   ########.fr       */
+/*   Updated: 2023/04/12 14:31:29 by aelandal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-int main(int ac, char **av) {
-    if (ac != 2) {
-        std::cout << "ERROR : check the number of argument" << std::endl;
-        return 0;
+bool isAllDigit(std::string str) {
+    for (int i = 0; str[i]; i++) {
+        if (isdigit(str[i]) == 0) {
+            std::cout << "-" << str[i] << "-" << std::endl;
+            std::cout << "ERROR" << std::endl;
+            exit(0);
+        }
     }
-    std::string str = av[1];
-    // for (int i = 0; str[i]; i++)
+    return 1;
+}
+
+void merge(std::vector<int>& vec, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    std::vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++)
+        L[i] = vec[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = vec[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            vec[k] = L[i];
+            i++;
+        }
+        else {
+            vec[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        vec[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        vec[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(std::vector<int>& vec, int left, int right) {
+    if (left < right) {
+        int m = left + (right - left) / 2;
+
+        mergeSort(vec, left, m);
+        mergeSort(vec, m + 1, right);
+
+        merge(vec, left, m, right);
+    }
+}
+
+
+std::vector<int> fillVector(char **av) {
+    std::vector<int> myVector;
+    std::string str;
+    for (int i = 1; av[i]; i++) {
+        str = av[i];
+        if (isAllDigit(str))
+            myVector.push_back(atoi(str.c_str()));
+    }
+    
+    return myVector;
+}
+        
+// int main(int ac, char **av) {
+//     if (ac < 2) {
+//         std::cout << "ERROR : check the number of argument" << std::endl;
+//         return 0;
+//     }
+//     std::vector<int> myVector = fillVector(av);
+//     // for (std::vector<int>::iterator it = myVector.begin(); it != myVector.end(); ++it)
+//     //     std::cout << *it << std::endl;
+//     return 0;
+
+int main() {
+    std::vector<int> vec;
+    vec.push_back(67);
+    vec.push_back(6);
+    vec.push_back(7);
+    vec.push_back(12);
+    vec.push_back(0);
+    vec.push_back(87);
+    vec.push_back(2);
+
+
+    int n = vec.size();
+
+    std::cout << "Given vector is: ";
+    for (int i = 0; i < n; i++)
+        std::cout << vec[i] << " ";
+    std::cout << std::endl;
+
+    mergeSort(vec, 0, n - 1);
+
+    std::cout << "Sorted vector is: ";
+    for (int i = 0; i < n; i++)
+        std::cout << vec[i] << " ";
+    std::cout << std::endl;
+
     return 0;
 }
+// }
